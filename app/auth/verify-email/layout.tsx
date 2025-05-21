202266e5e21789cf11059f60a71d9e17ab35ab96
Ahmadjonuz <karimovahmadjon2125@gmail.com>
2025-05-21 05:23:54 +0300

    Yangi ozgarishlar qoshildi va tuzatildi kamchiliklar

---
+++ app/auth/verify-email/layout.tsx
@@ -0,0 +1,13 @@
+import { Suspense } from "react"
+
+export default function VerifyEmailLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <Suspense>
+      {children}
+    </Suspense>
+  )
+} 
